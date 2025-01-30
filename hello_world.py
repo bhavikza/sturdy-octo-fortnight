4a8cecd0556f068a4b451e1d1bf474241712c438
bhavikza <79639058+bhavikza@users.noreply.github.com>
2025-01-30 16:27:06 +0000

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,13 @@
 # hello_world.py
+import os
+import datetime
+
 def main():
-    print("Hello, World!")
+    message = os.getenv('MESSAGE', 'No message provided')
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    print(f"Hello World at {current_time}!")
+    print(f"Received message: {message}")
 
 if __name__ == "__main__":
     main()
